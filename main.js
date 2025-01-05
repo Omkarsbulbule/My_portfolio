9baf19999e472974648dba03076dc48b206ee2b0
Omkar  S Bulbule <omkarbulbule7718@gmail.com>
2025-01-05 13:25:31 +0530

    files added

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed= new Typed(".text", {
+    strings:["Fronted Developer", "Web Developer" ," Engineer "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
+
+
+
+
+
